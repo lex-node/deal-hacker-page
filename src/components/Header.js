6d034d946d9116bbcc1c72028c4887e4c4bc720f
triggering-event <gabriel.j.shapiro@gmail.com>
2020-03-28 19:44:41 -0400

    miscellaneous fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-skull"></span>
+      <span className="icon fa-eye"></span>
     </div>
     <div className="content">
       <div className="inner">
