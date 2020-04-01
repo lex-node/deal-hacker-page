c75c5f0b89fdff1352eadb7027e6fdcb3196faf7
triggering-event <gabriel.j.shapiro@gmail.com>
2020-03-31 23:24:20 -0400

    credits codebushi in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">contents &copy; Gabriel J. Shapiro</p>
+      <br/>
+      <p className="copyright">design/code forked from <a href="https://github.com/codebushi/gatsby-starter-dimension">codebushi</a></p>
     </footer>
 )
 
