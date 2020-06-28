c97e345f3961a907d1471b82a1432f1d6aed4896
triggering-event <gabriel.j.shapiro@gmail.com>
2020-06-28 19:35:38 -0400

    updates to research page

--- src/pages/research.js
+++ src/pages/research.js
@@ -2,166 +2,206 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
-const Research = () => (
-  <Layout>
-    <Link to="/">Go back to the homepage</Link>
-    <h1 className="major"><strong>My CryptoLaw Thought</strong></h1>
-    <p>For several years, I have worked to articulate a set of interlinked tools and concepts forming a
-      kind of "cryptolaw philosophy".</p>
-    <p>The below articles, presentations and legal forms projects embody these ideas.</p>
-    <article>
-      <h3>
-        <strong>Blockchain Network Tokens Are Shares of Network Equity Subject to Modified Securities
-          Regulations</strong>
-        <br/>
-        -Blockchain network tokens are not primarily products, software licenses or currencies
-        (though they may secondarily have such functions), but shares of 'network equity.' They are
-        subject to securities laws regulations, unless the network has become sufficiently
-        decentralized. Sufficient decentralization occurs when the tokens no longer represent
-        investment contracts and no single party or group of affiliated parties objectively controls
-        the network equity or means of block production on the network <br/>
-        <ul>
-          <li><a
-            href="https://medium.com/@lex_node/defining-decentralization-for-law-58ca54e18b2a">An
+const Research = () => {
+  return (
+    <Layout>
+      <Link to="/">Go back to the homepage</Link>
+      <h1 className="major" style={{ color: `#1ec503`, margin: `20px` }}>Autonomous Law Research</h1>
+      <p style={{ margin: `30px`, color: `` }}>For years, I have worked to build a kind of groundwork for autonomous law
+        research. The below articles,
+        presentations and legal forms projects set forth these ideas:</p>
+
+      <div style={{
+        borderColor: `#ff2a6d`,
+        borderStyle: `solid`,
+        borderWidth: `1px`,
+        margin: `40px`,
+        marginRight: `150px`,
+        padding: `10px`,
+      }}>
+        <h3 style={{ color: `#1ec503`, marginBottom: `5px` }}>Blockchain Tokens as Shares of Network Equity</h3>
+        <p style={{ marginLeft: `10px`, marginTop: `5px`, marginBottom: '5px' }}>Blockchain network tokens are not
+          primarily products, software licenses or currencies
+          (though they may secondarily have such functions). They are shares of 'network equity.' Securities laws may
+          apply to them until the ecosystem has become sufficiently
+          decentralized. Sufficient decentralization occurs when the tokens no longer represent
+          investment contracts and no single party or group of affiliated parties controls
+          the network equity or means of block production on the network</p>
+        <ul style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+          <li style={{ color: `#a0ffe3` }}><a
+            href="https://medium.com/@lex_node/defining-decentralization-for-law-58ca54e18b2a">
             Defining Decentralization for Law</a></li>
-          <li><a
+          <li style={{ color: `#a0ffe3` }}><a
             href="https://www.theblockcrypto.com/post/55985/an-open-letter-to-sec-commissioner-peirce-on-token-safe-harbors">An
             Open Letter
             to SEC Commissioner Peirce On Token Safe Harbors</a></li>
-          <li><a href="https://medium.com/coinmonks/size-does-matter-part-1-9f83b130a451">Size
+          <li style={{ color: `#a0ffe3` }}><a href="https://medium.com/coinmonks/size-does-matter-part-1-9f83b130a451">Size
             Does Matter: A Philosophy of Securities Laws for Tokenized
             Networks Part
             1</a></li>
-          <li><a href="https://medium.com/coinmonks/size-does-matter-part-2-fbcb20671b53">Size
+          <li style={{ color: `#a0ffe3` }}><a href="https://medium.com/coinmonks/size-does-matter-part-2-fbcb20671b53">Size
             Does Matter: A Philosophy of Securities Laws for Tokenized
             Networks Part
             2</a></li>
-          <li><a href="https://medium.com/coinmonks/size-does-matter-part-3-b378244df7d5">Size
+          <li style={{ color: `#a0ffe3` }}><a href="https://medium.com/coinmonks/size-does-matter-part-3-b378244df7d5">Size
             Does Matter: A Philosophy of Securities Laws for Tokenized
             Networks Part
             3</a></li>
-          <li><a href="https://medium.com/coinmonks/size-does-matter-part-4-e66e2a242140/">Size
+          <li style={{ color: `#a0ffe3` }}><a href="https://medium.com/coinmonks/size-does-matter-part-4-e66e2a242140/">Size
             Does Matter: A Philosophy of Securities Laws for Tokenized
             Networks Part
             4</a></li>
-          <li>
+          <li style={{ color: `#a0ffe3` }}>
             <a
               href="https://medium.com/@lex_node/securities-compliance-paths-for-blockchain-projects-9f783d85a4aa?source=your_stories_page">Securities
               Compliance Paths for Blockchain Projects</a></li>
         </ul>
-      </h3>
-    </article>
-    <br/>
-    <h3>
-      <strong>Tokens as Securities Certificates</strong>
-      <br/>
-      - Tokens on public, permissionless blockchain networks function as transferable instruments
-      ('certificates') representing traditional and decentralized securities.<br/>
-      <ul>
-        <li><a href="https://gabrielshapiro.wordpress.com/2018/10/28/2/">Tokenizing
-          Corporate Capital Stock: Motivations and Potential Implementations</a></li>
-        <li><a href="https://www.wyoleg.gov/Legislation/2019/HB0185">Amendments to Wyoming's
-          Corporations Code</a>, authorizing Wyoming corporations to deploy tokenized stock
-          certificates. I advised and assisted the Wyoming Blockchain Task Force on the
-          drafting.
-        </li>
-        <li>The <a href="https://github.com/zerolawtech/ZAP-Tech">ZeroLaw
-          Org-Augmentation Protocol</a>, which implements a tokenized stock certificate system
-          on Ethereum
-        </li>
-      </ul>
-    </h3>
-    <br/>
-    <h3>
-      <strong>Qualified Legal Deference to Smart Contracts</strong>
-      <br/>
-      -Smart contracts deployed on public blockchain networks should be combined with
-      natural-language legal contracts under a synergistic "qualified code deference" methodology. This
-      will enable parties to agree to abide by the results of smart contracts except in unusual circumstances
-      <br/>
-      <ul>
-        <li><a href="https://github.com/metacartel/MCV/blob/master/MCV-Whitepaper.md">MetaCartel
-          Ventures Whitepaper</a></li>
-        <li>
-          <a
-            href="https://github.com/lex-node/SCoDA-Simple-Code-Deference-Agreement-/blob/master/DAO%20Charter%20with%20Qualified%20Code%20Deference.md">Model
-            DAO Charter for Unincorporated Associations</a></li>
-        <li>
-          <a href="https://github.com/lex-node/SCoDA-Simple-Code-Deference-Agreement-/blob/master/SCoDA%20v.3.md">Simple
-            Code Deference Agreement</a></li>
-        <li><a href="https://www.youtube.com/watch?v=hzbMPLxiht4">Drafting Wet Contracts in a
-          Smart Contract World (Video)</a></li>
-        <li><a href="https://www.youtube.com/watch?v=2eS5g4ttQFs&t=1164s">Black-Belt Blockchain
-          Drafting Jiu Jitsu (Video)</a></li>
-      </ul>
-    </h3>
-    <br/>
-    <h3>
-      <strong>Blockchain Governance</strong>
-      <br/>
-      -Blockchain governance is best understood through the lens of <a
-      href="https://twitter.com/nickszabo4/status/1009996445280169985?lang=en">"Szabo's Governance
-      Trilemma"</a>.
-      According to Nick Szabo, blockchain governance generally takes one of the following three forms:
-      <br/>
-      <ul>
-        <li><strong>ruthlessly minimized</strong></li>
-        <li><strong>lord of the flies</strong></li>
-        <li><strong>lawyers</strong></li>
-      </ul>
-      Each horn of the trilemma has pros and cons. Ruthlessly minimized governance, sometimes described as
-      following "Szabo's Law", is the most robust against certain forms of social capture. However, when
-      minimized governance is not desired or possible, my position is that legalistic governance is
-      preferable to lord-of-the-flies governance. If a network will be subject to frequent hardforks
-      which adversely affect minorities, due process is required and legalistic governance becomes
-      particularly important.
-      <br/>
-      <ul>
-        <li>
-          <a
-            href="https://medium.com/coinmonks/in-defense-of-szabos-law-for-a-mostly-non-legal-crypto-system-8f1ce2b364a6">In
-            Defense of Szabo's Law, For a (Mostly) Non-Legal Crypto System</a></li>
-        <li>
-          <a
-            href="https://medium.com/@lex_node/supplemental-rebuttal-to-angela-walchs-views-on-software-devs-as-fiduciaries-f886f2b47ffd">Supplemental
-            Rebuttal to Angela Walch's Views on Software Devs as Fiduciaries</a></li>
-        <li>
-          <a href="https://zips.z.cash/zip-1007">ZCash Improvement Proposal (ZIP) 1007:
-            Enforce Development Fund Commitments with a Legal Charter</a></li>
-        <li>
-          <a href="https://forum.zcashcommunity.com/t/major-grants-committee/35871/111?u=lex_node">Follow-Up
-            to ZIP 1007:
-            Governance Perspectives and Possibility of Creating ZCash-Specific Private Regulatory
-            Entity</a></li>
-      </ul>
-    </h3>
-    <br/>
-    <h3>
-      <strong>Podcast & Event Appearances</strong>
-      <br/>
-      -I have made appearances at various events and on podcasts.
-      <br/>
-      <ul>
-        <li>
-          <a href="https://www.buzzsprout.com/314447/2601559">Security Token Stories Episode
-            #14 - "DAOs, Tokenized Securities, and Sufficient Decentralization"</a></li>
-        <li>
-          <a
-            href="https://anchor.fm/wizardofdapps/episodes/Episode-29-Venture-DAO-with-Ameen-Soleimani-and-Gabriel-Shapiro-ea9i3p">Wizard
-            of Dapps Episode 29: Venture DAO with Ameen Soleimani and Gabriel Shapiro</a>
-        </li>
-        <li>
-          <a href="http://www.cryptolawpodcast.com/episodes/50-foot/">CryptoLaw Podcast (w/
-            David Gerard)</a></li>
-        <li>
-          <a
-            href="http://www.cryptolawpodcast.com/episodes/the-cryptolaw-podcast-interview-with-lewis-cohen-gabriel-shapiro/">CryptoLaw
-            Podcast (w/ Lewis Cohen)</a></li>
-      </ul>
-    </h3>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+      </div>
+
+      <div style={{
+        borderColor: `#ff2a6d`,
+        borderStyle: `solid`,
+        borderWidth: `1px`,
+        margin: `40px`,
+        marginRight: `150px`,
+        padding: `10px`,
+      }}>
+        <h3 style={{ color: `#1ec503`, marginBottom: `5px` }}>Tokens as Securities Certificates</h3>
+        <p style={{ marginLeft: `10px`, marginTop: `5px`, marginBottom: '5px' }}>Tokens on public, permissionless
+          blockchain networks function as transferable instruments
+          ('certificates') representing traditional and decentralized securities.</p>
+        <ul style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+          <li style={{ color: `#a0ffe3` }}><a href="https://gabrielshapiro.wordpress.com/2018/10/28/2/">Tokenizing
+            Corporate Capital Stock: Motivations and Potential Implementations</a></li>
+          <li style={{ color: `#a0ffe3` }}><a href="https://www.wyoleg.gov/Legislation/2019/HB0185">Amendments to
+            Wyoming's
+            Corporations Code</a>, authorizing Wyoming corporations to deploy tokenized stock
+            certificates. I advised and assisted the Wyoming Blockchain Task Force on the
+            drafting.
+          </li>
+          <li style={{ color: `#a0ffe3` }}>The <a href="https://github.com/zerolawtech/ZAP-Tech">ZeroLaw
+            Org-Augmentation Protocol</a>, which implements a tokenized stock certificate system
+            on Ethereum
+          </li>
+        </ul>
+      </div>
+
+      <div style={{
+        borderColor: `#ff2a6d`,
+        borderStyle: `solid`,
+        borderWidth: `1px`,
+        margin: `40px`,
+        marginRight: `150px`,
+        padding: `10px`,
+      }}>
+        <h3 style={{ color: `#1ec503`, marginBottom: `5px` }}>Qualified Code Deference for Legal Agreements</h3>
+        -Smart contracts deployed on public blockchain networks should be combined with
+        natural-language legal contracts under a synergistic "qualified code deference" methodology. This
+        will enable parties to agree to abide by the results of smart contracts except in unusual circumstances
+        <ul style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+          <li style={{ color: `#a0ffe3` }}><a href="https://github.com/metacartel/MCV/blob/master/MCV-Whitepaper.md">MetaCartel
+            Ventures Whitepaper</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a
+              href="https://github.com/lex-node/SCoDA-Simple-Code-Deference-Agreement-/blob/master/DAO%20Charter%20with%20Qualified%20Code%20Deference.md">Model
+              DAO Charter for Unincorporated Associations</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://github.com/lex-node/SCoDA-Simple-Code-Deference-Agreement-/blob/master/SCoDA%20v.3.md">Simple
+              Code Deference Agreement</a></li>
+          <li style={{ color: `#a0ffe3` }}><a href="https://www.youtube.com/watch?v=hzbMPLxiht4">Drafting Wet Contracts
+            in a
+            Smart Contract World (Video)</a></li>
+          <li style={{ color: `#a0ffe3` }}><a href="https://www.youtube.com/watch?v=2eS5g4ttQFs&t=1164s">Black-Belt
+            Blockchain
+            Drafting Jiu Jitsu (Video)</a></li>
+        </ul>
+      </div>
+
+      <div style={{
+        borderColor: `#ff2a6d`,
+        borderStyle: `solid`,
+        borderWidth: `1px`,
+        margin: `40px`,
+        marginRight: `150px`,
+        padding: `10px`,
+      }}>
+        <h3 style={{ color: `#1ec503`, marginBottom: `5px` }}>Blockchain Governance</h3>
+        <p style={{ marginLeft: `10px`, marginTop: `5px`, marginBottom: '5px' }}>Blockchain governance is best
+          understood through the lens of <a
+            href="https://twitter.com/nickszabo4/status/1009996445280169985?lang=en">"Szabo's Governance
+            Trilemma"</a>.
+          According to Nick Szabo, blockchain governance generally takes one of the following three forms:
+          <ol style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+            <li>ruthlessly minimized</li>
+            <li>lord of the flies</li>
+            <li>lawyers</li>
+          </ol>
+          Each horn of the trilemma has pros and cons. Ruthlessly minimized governance, sometimes described as
+          following "Szabo's Law", is the most robust against certain forms of social capture. However, when
+          minimized governance is not desired or possible, legalistic governance is
+          preferable to lord-of-the-flies governance. If a network will be subject to frequent hardforks
+          which adversely affect minorities, due process is required and legalistic governance becomes
+          particularly important.
+        </p>
+        <ul style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+          <li style={{ color: `#a0ffe3` }}>
+            <a
+              href="https://medium.com/coinmonks/in-defense-of-szabos-law-for-a-mostly-non-legal-crypto-system-8f1ce2b364a6">In
+              Defense of Szabo's Law, For a (Mostly) Non-Legal Crypto System</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a
+              href="https://medium.com/@lex_node/supplemental-rebuttal-to-angela-walchs-views-on-software-devs-as-fiduciaries-f886f2b47ffd">Supplemental
+              Rebuttal to Angela Walch's Views on Software Devs as Fiduciaries</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://zips.z.cash/zip-1007">ZCash Improvement Proposal (ZIP) 1007:
+              Enforce Development Fund Commitments with a Legal Charter</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://forum.zcashcommunity.com/t/major-grants-committee/35871/111?u=lex_node">Follow-Up
+              to ZIP 1007:
+              Governance Perspectives and Possibility of Creating ZCash-Specific Private Regulatory
+              Entity</a></li>
+        </ul>
+      </div>
+
+      <div style={{
+        borderColor: `#ff2a6d`,
+        borderStyle: `solid`,
+        borderWidth: `1px`,
+        margin: `40px`,
+        marginRight: `150px`,
+        padding: `10px`,
+      }}>
+        <h3 style={{ color: `#1ec503`, marginBottom: `5px` }}>Podcast & Event Appearances</h3>
+        -I have made appearances at various events and on podcasts.
+        <ul style={{ marginLeft: `40px`, marginBottom: `5px` }}>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://www.youtube.com/watch?v=S-U38LIslVE">Epicenter #328: Gabriel Shapiro - A Philosophy of
+              Securities Laws for Tokenized Networks</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://www.youtube.com/watch?v=MQqGC3HTxWQ">HODLCast Ep. 115 with Atty. Gabriel Shapiro on The
+              Bright LIne Test for Sufficient Deentralization</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="https://www.buzzsprout.com/314447/2601559">Security Token Stories Episode
+              #14 - "DAOs, Tokenized Securities, and Sufficient Decentralization"</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a
+              href="https://anchor.fm/wizardofdapps/episodes/Episode-29-Venture-DAO-with-Ameen-Soleimani-and-Gabriel-Shapiro-ea9i3p">Wizard
+              of Dapps Episode 29: Venture DAO with Ameen Soleimani and Gabriel Shapiro</a>
+          </li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a href="http://www.cryptolawpodcast.com/episodes/50-foot/">CryptoLaw Podcast (w/
+              David Gerard)</a></li>
+          <li style={{ color: `#a0ffe3` }}>
+            <a
+              href="http://www.cryptolawpodcast.com/episodes/the-cryptolaw-podcast-interview-with-lewis-cohen-gabriel-shapiro/">CryptoLaw
+              Podcast (w/ Lewis Cohen)</a></li>
+        </ul>
+      </div>
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 
 export default Research
